e1089766d054fa7f5c076f9d336e8d33b7aa673b
Bastian Kuolt <Bastian.Kuolt@gmail.com>
2014-05-02 09:52:48 +0200

    Initial Draft

---
+++ Charcater.h
@@ -0,0 +1,78 @@
+
+
+class Character
+{
+	public:
+		//
+		typedef Level::Position Position;
+		typedef Position::Coordinate Coordinate;
+
+		/*
+		 *
+		 */
+		void goTo(const Position& position)
+		{
+		
+			// Check 
+		
+		
+		
+			if(position.x == position.
+		
+			// Testen,ob überhaupt möglich)
+		
+			// Nur Springen
+			// Nur walk:
+			
+			// Beides jump and walk(wiederholt):
+			//
+		}
+		
+		
+		
+		
+	protected:
+		/*
+		 *
+		 */
+		void walkTo(Coordinate x)
+		{
+			if(position.x == x)
+			{
+				
+			}
+			else if(position.x < x)
+			{
+			
+			}
+			else (position.x > x)
+			{
+			
+			}
+			// Stand am Ende in die Animation Queue
+		}
+	
+	private:
+	
+	
+	
+		// Animations bedingt
+		
+		
+		void notifyWalk(int x)
+		void notifyStand()
+		// notifyJump()
+		//
+		
+}
+
+
+
+
+
+
+
+
+
+
+
