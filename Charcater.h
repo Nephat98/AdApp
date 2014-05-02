743629dd76427de5b5710679a6c25df2324df926
Bastian Kuolt <Bastian.Kuolt@gmail.com>
2014-05-02 10:57:02 +0200

    Initial draft

--- Charcater.h
+++ Charcater.h
@@ -1,14 +1,49 @@
 
+namespace bgl {
 
+/*
+ *
+ */
 class Character
 {
 	public:
 		//
 		typedef Level::Position Position;
 		typedef Position::Coordinate Coordinate;
-
+		
+		
+	private:
+	
 		/*
-		 *
+		 * Führt Befehle aus
+		 */
+		void walkTo(Coordinate x)
+		{
+			// Checks if a current route to be cancelled exists
+			{
+				// Cancels current command
+			}
+			
+			// Issues commands
+			
+			if(position.x == x)
+			{
+				// does nothing
+			}
+			else // Walk Left oder right
+			{
+				// Computes Walking Time 
+				
+				// Starts animations
+				if(position.x > x)
+				else 
+			}
+	
+		}
+
+	public:
+		/*
+		 * Checkt policies
 		 */
 		void goTo(const Position& position)
 		{
@@ -28,43 +63,167 @@ class Character
 			//
 		}
 		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		Position position;
 		
-		
-		
-	protected:
 		/*
 		 *
 		 */
-		void walkTo(Coordinate x)
+		class Movement
 		{
-			if(position.x == x)
-			{
+			public:
+				void setAnimation
+				void setCallback
 				
-			}
-			else if(position.x < x)
-			{
+			private:
+				notify
+				notify
+		};
+		
+		/*
+		 *
+		 */
+		class MovementPolicy
+		{
+		
+		};
+
+				
+		// TODO: Default Policy;
+		
+
+		
+		void setMovementPolicy(
+		void setMove
+
+		
+		
+	public:
+	
+
+		
+		
+		
+	
+		
+	private:
+		std::queue<std::function<void(void)>> moveQueue;
+		
+	
 			
-			}
-			else (position.x > x)
-			{
 			
-			}
 			// Stand am Ende in die Animation Queue
 		}
-	
-	private:
-	
-	
-	
+
 		// Animations bedingt
 		
+		void notifyWalkBegin(void)
+		void notifyWalkEnd(void)
 		
-		void notifyWalk(int x)
-		void notifyStand()
-		// notifyJump()
-		//
+		void notifyStandBegin(void) // Begin und end sind gleich am Anfang
+		void notifyStandEnd(void)
 		
 }
+
+#ifdef
+
+void CreateCharacter
+{
+
+	Character character;
+	
+	// TODO: Registers all moves
+
+	
+	// Sets the move policy
+	MovePolicy movePolicy;
+	
+	movePolicy->setMove
+
+	character->setMovePolicy(movePolicy);
+	
+	
+	return charcater;
+}
+
+#end
+
+
+
+
+
+/*
+ *
+ */
+class WalkCommand : public MoveCommand
+{
+	public
+		virtual void perform(void)
+		{}
+		
+	private:
+		{
+		
+			// start Animation
+		}
+		
+}
+
+// dgsssf
+
+
+
+
+
+
+// Auswahl: Policy gefragt
+Laufen in eine ungültige Position
+	(1) Zulassen?
+	(2) nahe gültige Position suchen
+	
+	(3) Ausrechnen wie lange Animation gehen wird
+	(3) Animation starten (in Liste packen)
+	(3) Animation starten
+	
+	
+	
+Laufen und dann richtung wechseln in eine ungültige Position
+
+
+
+//Alles Perfekt
+Laufen:
+	
+	LaufAnimation 
+Laufen und dann Richtung wechseln
+
+class AnimationQueue
+{
+
+}
+
+quue.next();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
